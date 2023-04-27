e25d55151c4df2e550e8d9042fbdc66c13ca2ac8
Mezzichai <76605204+Mezzichai@users.noreply.github.com>
2023-04-27 09:31:35 -0600

    starting the toolbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
 import NavStyles from "./NavStyles.module.css"
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-// import { fa } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlug, faPlus, faX} from '@fortawesome/free-solid-svg-icons'
 
 
 const Nav = () => {
@@ -16,6 +15,12 @@ const calcArr = [
 ]
 
 const [tools, setTools] = useState(null)
+const [addCalcBtn, setAddCalcBtn] = useState(false)
+const [calcName, setCalcName] = useState('')
+
+const handleAddCalc = () => {
+  addCalcBtn ? setAddCalcBtn(false) : setAddCalcBtn(true)
+}
 
   return (
     <nav className={NavStyles.nav}>
@@ -27,6 +32,14 @@ const [tools, setTools] = useState(null)
            {calcArr.map((calc) => {
             return <li className={NavStyles.element} key={calc.name}>{calc.name}</li>
            })}
+           {!addCalcBtn ? (
+            <button onClick={handleAddCalc} className={NavStyles.AddCalc}><FontAwesomeIcon icon={faPlus}/></button>
+           ) : (
+            <form className={NavStyles.nameCalcForm}>
+              <input className={NavStyles.nameCalc} onChange={(e)=>{setCalcName(e.target.value)}} type="text" value={calcName}/>
+              <button className={NavStyles.exitNewCalc}><FontAwesomeIcon icon={faX}/></button>
+            </form>
+           )}
         </ul>
     </nav>
   )
